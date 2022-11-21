4d93e6eeb8385b67b6a874b0a32dcf37f52bc9b6
mahircetin <ctn.mahir@gmail.com>
2022-11-21 23:36:45 +0300

    Add precision selector for undefined ticker

--- resources/sources/fetch.js
+++ resources/sources/fetch.js
@@ -5,7 +5,7 @@ const { get, find, isUndefined } = require("lodash");
 const precisions = require("../assets/precisions");
 
 module.exports = async ({ request, asset, binance }) => {
-	let margin;
+	let margin, precision;
 
 	const ticker = get(request, "body.ticker", "BTCBUSD");
 	const risk = get(request, "body.risk", 1);
@@ -28,14 +28,21 @@ module.exports = async ({ request, asset, binance }) => {
 		margin = (((balance * risk) / 100) * leverage) / price;
 	}
 
-	const precision = precisions[ticker];
-	const quantity = Math.round(margin * 10 ** precision) / 10 ** precision;
+	if (isUndefined(precisions[ticker]) === true) {
+		const exchange = await binance.futuresExchangeInfo();
+		const symbols = get(exchange, "symbols", []);
+		const symbol = find(symbols, (i) => i.symbol === ticker);
+		
+		precision = get(symbol, "quantityPrecision", 0);
+	} else {
+		precision = precisions[ticker];
+	}
 
 	return {
 		type: get(request, "body.type", "MARKET"),
 		side: get(request, "body.side", "BUY"),
 		ticker,
-		quantity,
+		quantity: Math.round(margin * 10 ** precision) / 10 ** precision,
 		price,
 		stop: get(request, "body.params.stop"),
 		profit: get(request, "body.params.profit"),
